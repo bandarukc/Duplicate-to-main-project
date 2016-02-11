365402a455754e3c6741584e6207041cde373722
bandarukc <bandarukc@gmail.com>
2016-02-11 22:55:27 +0530

    initial commit

---
+++ duplicate for main project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="duplicate_for_main_project.MvcApplication" Language="C#" %>
---
+++ duplicate for main project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(duplicate_for_main_project.Startup))]
+namespace duplicate_for_main_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
